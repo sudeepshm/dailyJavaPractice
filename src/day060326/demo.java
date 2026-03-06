eec2aedd2204daaec9ffa253cb7841b54c9c9405
sude2033 <ssud335@gmail.com>
2026-03-06 22:45:34 +0530

    03032026 practised

---
+++ src/day060326/demo.java
@@ -0,0 +1,23 @@
+package day060326;
+class A{
+    public void show(){
+        System.out.println("in a show");
+    }
+    public void config(){
+        System.out.println("in a config");
+    }
+}
+class B extends A {
+    public void show(){
+        System.out.println("in b  show");
+    }
+}
+
+
+public class demo {
+    public static void main(String[] args) {
+        B obj=new B();
+        obj.show();
+        obj.config();
+    }
+}
