3338faf084f22242968ade839f0df066e755170a
sude2033 <ssud335@gmail.com>
2026-03-11 22:37:21 +0530

    daily practise

---
+++ src/day110326/bubbleSort.java
@@ -0,0 +1,27 @@
+package day110326;
+
+public class bubbleSort {
+    public static void main(String[] args) {
+        int nums[]={4,2,8,5,9,2};
+        int size=nums.length;
+        int temp;
+        System.out.println("before sorting");
+        for(int num:nums){
+            System.out.print(num+" ");
+        }
+        for(int i=0;i<size;i++){
+            for(int j=0;j<size-i-1;j++){
+                if(nums[j]>nums[j+1]){
+                    temp=nums[j];
+                    nums[j]=nums[j+1];
+                    nums[j+1]=temp;
+                }
+            }
+        }
+        System.out.println("after sorting");
+        System.out.println();
+        for(int num:nums){
+            System.out.println(num);
+        }
+    }
+}
