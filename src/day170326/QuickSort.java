cbf17ea26825d45bbba1f17c2bfc392871b9324f
sude2033 <ssud335@gmail.com>
2026-03-17 21:38:42 +0530

    daily practise

---
+++ src/day170326/QuickSort.java
@@ -0,0 +1,36 @@
+package day170326;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {10, 7, 8, 9, 1, 5};
+        sort(arr, 0, arr.length - 1);
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+
+    static void sort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            sort(arr, low, pi - 1);
+            sort(arr, pi + 1, high);
+        }
+    }
+
+    static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+}
